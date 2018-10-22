8e63746367102ed7e7f968aa721f80c85e890194
Andrii Berezniuk <andrii.berezniuk@gmail.com>
2018-10-22 19:51:28 +0300

    update

--- docker/app/main.go
+++ docker/app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Softserve!! hello: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Softserve!!: %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":80", nil)
